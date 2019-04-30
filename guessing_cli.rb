823ad81eb0aa91634e8e9283d4d90777e5de9c50
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-30 21:41:29 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,15 +1,19 @@
 require "pry"
 
 def run_guessing_game
-  puts "Guess a number between 1 and 6."
-  guess = gets.chomp
-  random = rand(6)+1
-  if guess == "exit"
-    puts "Goodbye!"
-  end
-  if guess == random
-    puts "You guessed the correct number!"
-  else
-    puts "The computer guessed #{random}."
+  loop do
+    puts "Guess a number between 1 and 6."
+    guess = gets.chomp
+    random = rand(6)+1
+    if guess == "exit"
+      puts "Goodbye!"
+      break
+    end
+    if guess == random
+      puts "You guessed the correct number!"
+      break
+    else
+      puts "The computer guessed #{random}."
+    end
   end
 end
