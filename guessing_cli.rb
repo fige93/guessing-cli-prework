a945dc65c07f3ac09d7288ebf8cf34d411f5fa0a
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-30 21:43:41 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   loop do
     puts "Guess a number between 1 and 6."
     guess = gets.chomp
-    random = rand(6)+1
+    random = rand(1..6)
     if guess == "exit"
       puts "Goodbye!"
       break
